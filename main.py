b6254a532c2c3639f14626ee77d8efe0ce6bd873
osadchukstepa-dev <osadchuk.stepa@gmail.com>
2026-02-20 16:03:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ if 'b' not in st.session_state:
     st.session_state.b = 1000   # Баланс
 if "nickname" not in st.session_state:
     st.session_state.nickname = ""
-pg_reg = st.Page("pages/project.py", title="Регистрация")
-pg_home = st.Page("pages/page_2.py", title="Главная")
-pg_kredits = st.Page("pages/kredits.py", title="Кредиты")
+pg_reg = st.Page("project.py", title="Регистрация")
+pg_home = st.Page("page_2.py", title="Главная")
+pg_kredits = st.Page("kredits.py", title="Кредиты")
 
 pg = st.navigation([pg_reg, pg_home, pg_kredits])
 
 pg.run()
+
