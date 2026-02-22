70bdd0c912d5f63a7009dad5fd6907204b2d4729
osadchukstepa-dev <osadchuk.stepa@gmail.com>
2026-02-22 11:26:31 +0500

    Update project.py

--- project.py
+++ project.py
@@ -1,71 +1,53 @@
 import streamlit as st
-import streamlit as st
-import json
-import os
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 
-user_name = st.session_state.get("user_name")
-cookie_manager = st.session_state.get("cookie_manager")
-
+# 1. Подключение к базе
 conn = st.connection("gsheets", type=GSheetsConnection)
-
-def load_cloud_data():
-    return conn.read(ttl=0).dropna(how="all")
+df = conn.read(ttl=0).dropna(how="all")
 
 def save_cloud_data(updated_df):
     conn.update(data=updated_df)
     st.cache_data.clear()
 
-# Загружаем таблицу пользователей из облака
-df = load_cloud_data()
-
+# 2. ЗАБИРАЕМ данные из "общего ящика" (session_state)
+user_name = st.session_state.get("user_name")
+cookie_manager = st.session_state.get("cookie_manager")
 
 st.title("Личный кабинет")
 
+# Если пользователь авторизован
 if user_name:
-    # Ищем пользователя в облачной таблице (df)
     user_row = df[df['name'] == user_name]
     
     if not user_row.empty:
         st.write(f"С возвращением, **{user_name}**!")
-        # Тут можно выводить баланс или кредиты из таблицы
+        # Исправил вывод баланса (используем .iloc[0])
         st.write(f"Ваш баланс: {user_row.iloc[0]['balance']} ₽")
     else:
-        st.warning("Вы в системе, но данных в облаке нет. Перезайдите.")
+        st.warning("Аккаунт не найден в таблице.")
         if st.button("Выйти"):
-            cookie_manager.delete("user_name", key="delete_user")
+            cookie_manager.delete("user_name") # Работает, т.к. мы взяли его из session_state
             st.rerun()
 
 else:
-    # 3. Регистрация/Вход (теперь сохраняем в Google Sheets, а не в JSON)
+    # 3. Регистрация
     st.subheader("Авторизация")
     nickname = st.text_input("Как тебя зовут?", key="reg_name")
     password = st.number_input("Введите пароль", value=0, step=1, key="reg_pass")
     
     if st.button("Войти или Создать аккаунт", key="reg_btn"):
         if nickname:
-            # Проверяем, есть ли человек в ОБЛАЧНОЙ таблице
             if nickname not in df['name'].values:
-                # Создаем новую строку для таблицы
                 new_user = pd.DataFrame([{
-                    "name": nickname,
-                    "password": password,
-                    "balance": 60000,
-                    "loans": "[]"
+                    "name": nickname, "password": password, "balance": 60000, "loans": "[]"
                 }])
-                # Объединяем старую таблицу с новым юзером
                 df = pd.concat([df, new_user], ignore_index=True)
-                # Сохраняем В ОБЛАКО (Google Sheets)
                 save_cloud_data(df)
-                st.success(f"Аккаунт {nickname} создан в облаке!")
-            else:
-                st.info("Вход в существующий аккаунт...")
-
-            # Устанавливаем куку
-            cookie_manager.set("user_name", nickname, key="set_cookie_name")
-            st.info("Нажми еще раз или обнови страницу.")
+                st.success(f"Аккаунт {nickname} создан!")
+            
+            # Сохраняем куку через менеджер из main.py
+            cookie_manager.set("user_name", nickname, key="save_user_cookie")
+            st.info("Готово! Переключите страницу или обновите её.")
         else:
             st.error("Введите имя!")
-
-
