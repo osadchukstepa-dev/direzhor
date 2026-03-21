68c26d415048ea5e8abce13530e126a3a6921035
osadchukstepa-dev <osadchuk.stepa@gmail.com>
2026-03-21 14:41:02 +0500

    Update kredits.py

--- kredits.py
+++ kredits.py
@@ -1,68 +1,40 @@
-
 import streamlit as st
 import extra_streamlit_components as stx
-import time
-from datetime  import date
 import json
-import os
-from project import *
-from datetime import datetime, timedelta
-kredits_cookies = cookie_manager.get(cookie="kredits_cookies")
+import time
+import os  # Добавили импорт!
+from datetime import date, datetime, timedelta
 
+# 1. Менеджер создается ТОЛЬКО ОДИН РАЗ в самом верху
+if 'cookie_manager' not in st.session_state:
+    st.session_state.cookie_manager = stx.CookieManager(key="bank_mngr")
+
+cookie_manager = st.session_state.cookie_manager
+
+# Файловые функции
 DB_FILE_1 = "data.json"
 def load_data():
     if not os.path.exists(DB_FILE_1):
-        # Начальные данные, если файла еще нет
         initial_data = {"balance": 60000}
         with open(DB_FILE_1, "w") as f:
             json.dump(initial_data, f)
         return initial_data
-    
     with open(DB_FILE_1, "r") as f:
         return json.load(f)
 
-# Функция для сохранения данных
 def save_data(data):
     with open(DB_FILE_1, "w") as f:
         json.dump(data, f, indent=4)
 
-
-# Загружаем актуальное состояние
 data = load_data()
 balance = data["balance"]
 
-
-
-cookie_manager = stx.CookieManager()
-
-# Нужно также загрузить базу пользователей, чтобы было куда записывать кредит
-def load_db():
-    if os.path.exists("users_stats.json"): # Убедись, что имя файла совпадает с тем, где юзеры
-        with open("users_stats.json", "r") as f:
-            return json.load(f)
-    return {}
-
-def save_db(data):
-    with open("users_stats.json", "w") as f:
-        json.dump(data, f, indent=4)
-
-db = load_db()
+# Получаем данные пользователя
 user_name = cookie_manager.get(cookie="user_name")
 
-
 procen = {
-    "Январь": 31,
-    "Февраль": 28, # В високосный год 29
-    "Март": 31,
-    "Апрель": 30,
-    "Май": 31,
-    "Июнь": 30,  # Исправил порядок и дни
-    "Июль": 31,
-    "Август": 31,
-    "Сентябрь": 30, # Добавил значение (было пусто)
-    "Октябрь": 31,
-    "Ноябрь": 30,
-    "Декабрь": 31
+    "Январь": 31, "Февраль": 28, "Март": 31, "Апрель": 30, "Май": 31, "Июнь": 30,
+    "Июль": 31, "Август": 31, "Сентябрь": 30, "Октябрь": 31, "Ноябрь": 30, "Декабрь": 31
 }
 
 month_to_num = {
@@ -71,101 +43,83 @@ month_to_num = {
 }
 
 st.title("Кредит")
-
 st.warning(f"Доступно {balance}")
+
 kredit = st.number_input("Выберете сумму кредита", min_value=300, max_value=60000)
-st.write(" ")
 
-col1, col2  = st.columns(2)
-
+col1, col2 = st.columns(2)
 with col1:
-    month = st.selectbox("Выбери месяц начала кредита", list(procen.keys()))
+    month = st.selectbox("Выбери месяц начала", list(procen.keys()))
 with col2:
-    max_days = procen[month]
-    days = st.selectbox("Выбери день начала кредита", list(range(1, max_days + 1)))
-
-
-st.write(" ")
-
+    days = st.selectbox("Выбери день начала", list(range(1, procen[month] + 1)))
 
 col3, col4 = st.columns(2)
-
 with col3:
-    month_finish = st.selectbox("Выбери месяц конца кредита", list(procen.keys()))
+    month_finish = st.selectbox("Выбери месяц конца", list(procen.keys()))
 with col4:
-    max_days_finish = procen[month_finish]
-    days_finish = st.selectbox("Выбери день конца кредита", list(range(1, max_days_finish + 1)))
-
-st.title(f"{month}, {days} ------ {month_finish}, {days_finish}")
+    days_finish = st.selectbox("Выбери день конца", list(range(1, procen[month_finish] + 1)))
 
 d_start = date(2026, month_to_num[month], days)
 d_end = date(2026, month_to_num[month_finish], days_finish)
-
 delta = d_end - d_start
 loan_days = delta.days
 
 if loan_days <= 0:
     st.error("Ошибка: Дата конца должна быть позже даты начала!")
 else:
-    
-    base_rate = 0.05
-    total_interest = kredit * (base_rate / 30) * loan_days
+    total_interest = kredit * (0.05 / 30) * loan_days
     st.metric(label="Переплата", value=f"{round(total_interest, 2)} ₽")
     
     @st.dialog("Кредитный договор")
     def show_popup():
-        new_loan = st.text_input("введите название кредита")
-        st.write("Проставьте все галочки для подтверждения:")
-        c1 = st.checkbox("я обязуюсь оплатить кредит с комиссией")
-        c2 = st.checkbox("я оставляю под залог")
-        c3 = st.checkbox("согласен на изъятие залога при неуплате")
-        c4 = st.checkbox("согласен на срок 1 месяц для закрытия")
-        c5 = st.checkbox("согласен на начисление штрафных процентов")
+        new_loan = st.text_input("Введите название кредита")
+        c1 = st.checkbox("Обязуюсь оплатить")
+        c2 = st.checkbox("Оставляю залог")
+        c3 = st.checkbox("Согласен на изъятие")
+        c4 = st.checkbox("Срок 1 месяц")
+        c5 = st.checkbox("Согласен на штрафы")
         
         if st.button("Подтвердить и взять кредит"):
-            # ... внутри диалога подтверждения кредита ...
             if all([c1, c2, c3, c4, c5]) and user_name:
-                cookie_manager.set(
-                "kredits_cookies", 
-                json.dumps(loans_list), 
-                key=f"save_loan_{time.time()}" # Уникальный ключ при каждом нажатии
-                )
-
-                old_cookies = cookie_manager.get(cookie="kredits_cookies")
+                # Читаем старые куки
+                current_raw = cookie_manager.get(cookie="kredits_cookies")
                 
-                # Парсим старое (если это строка)
-                if isinstance(old_cookies, str):
-                    try: loans_list = json.loads(old_cookies)
-                    except: loans_list = []
-                else:
-                    loans_list = old_cookies if old_cookies else []
-            
-                # 2. Добавляем новый кредит
-                new_data = {
-                    "name": new_loan,
+                try:
+                    if current_raw:
+                        loans_list = json.loads(current_raw) if isinstance(current_raw, str) else current_raw
+                    else:
+                        loans_list = []
+                except:
+                    loans_list = []
+
+                # Добавляем новый
+                new_entry = {
+                    "name": new_loan if new_loan else "Без названия",
                     "amount": kredit,
                     "date_end": str(d_end),
-                    "repayment": round(kredit + total_interest, 2)
+                    "repayment": round(kredit + total_interest, 2),
+                    "stats": "+"
                 }
-                loans_list.append(new_data)
-            
-                # 3. ЗАПИСЫВАЕМ (обязательно через json.dumps!)
+                loans_list.append(new_entry)
+
+                # Сохраняем
                 cookie_manager.set(
                     "kredits_cookies", 
                     json.dumps(loans_list), 
-                    key="save_action_unique" # Ключ должен быть уникальным!
+                    key=f"save_{time.time()}"
                 )
                 
-                st.success("Данные отправлены в браузер...")
-                time.sleep(2) # ДАЙ БРАУЗЕРУ 2 СЕКУНДЫ, ЧТОБЫ ПРИНЯТЬ КУКУ
+                # Уменьшаем баланс в файле (банка)
+                data["balance"] -= kredit
+                save_data(data)
+                
+                st.success("Готово!")
+                time.sleep(2)
                 st.rerun()
-
-
             elif not user_name:
-                st.error("Ошибка: вы не авторизованы!")
+                st.error("Войдите в аккаунт!")
             else:
-                st.warning("Нужно отметить все пункты!")
+                st.warning("Отметьте все пункты!")
 
     if st.button("Оформить кредит"):
         show_popup()
-    
