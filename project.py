0fe0db82952d19cccf5620ec931b0037bdbdd88f
osadchukstepa-dev <osadchuk.stepa@gmail.com>
2026-05-17 15:02:55 +0500

    Update project.py

--- project.py
+++ project.py
@@ -1,49 +1,22 @@
-
 import streamlit as st
-import extra_streamlit_components as stx
-import json
-import os
 
-DB_FILE_1 = "users_stats.json"
-
-def load_db():
-    if os.path.exists(DB_FILE_1):
-        with open(DB_FILE_1, "r") as f:
-            return json.load(f)
+# 1. Подключаем глобальную память прямо на странице регистрации
+@st.cache_resource
+def get_global_db():
     return {}
 
-def save_db(data):
-    with open(DB_FILE_1, "w") as f:
-        json.dump(data, f, indent=4)
+global_db = get_global_db()
 
-db = load_db()
-cookie_manager = stx.CookieManager()
-
-# 1. Читаем куки
-user_name = cookie_manager.get(cookie="user_name")
-
-if user_name:
-
-    if user_name in db:
-        user_data = db[user_name]
-
-else:
-    # 2. Регистрация, если куки нет
-    nickname = st.text_input("Как тебя зовут?")
-    password = st.number_input("Введите пароль", value=0, step=1)
-    
-    if st.button("Войти или Создать аккаунт"):
-        if nickname:
-            # Если человека нет в базе — создаем
-            if nickname not in db:
-                db[nickname] = {
-                    "password": password,
-                }
-                save_db(db)
-                st.success("Новый аккаунт создан!")
-
-            # Сохраняем куку
-            cookie_manager.set("user_name", nickname, key="set_name")
-            st.success("Готово! Обновите страницу.")
-        else:
-            st.error("Введите имя!")
+# ... ваш существующий код ввода никнейма ...
+# Допустим, у вас есть кнопка входа:
+if st.button("Войти"):
+    if nickname_input: # если никнейм введен
+        st.session_state.nickname = nickname_input
+        
+        # 2. Обязательно записываем пользователя в глобальную базу!
+        global_db[st.session_state.nickname] = {
+            "balance": st.session_state.get("b", 1000),
+            "credit_limit": st.session_state.get("n", 60000),
+            "credit_taken": st.session_state.get("credit_taken", 0)
+        }
+        st.success("Вы успешно вошли!")
